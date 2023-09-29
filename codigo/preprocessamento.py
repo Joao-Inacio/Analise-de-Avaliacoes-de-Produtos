4f54bb9ea54c0098151d02eca843b758b687f8fc
Joao-Inacio <joaoinacio206@protonmail.com>
2023-09-29 15:26:00 -0300

    Script Python onde estão todas as funções de pre processamento

---
+++ codigo/preprocessamento.py
@@ -0,0 +1,27 @@
+
+# Função para converte csv em json
+def csv_para_json(arquivo):
+    import csv
+    with open(arquivo, 'r', encoding='utf-8', errors='replace') as arquivo_csv:
+        leitor = csv.DictReader(arquivo_csv)
+        dados = [linha for linha in leitor]
+    return dados
+
+
+# Função para adicionar dados no MongoDB
+def adicionar_dados(dados, bd_nome, collection_nome):
+    from pymongo import MongoClient
+    cliente = MongoClient('127.0.0.1:27017')
+    bd = cliente[bd_nome]
+    collection = bd[collection_nome]
+    collection.insert_many(dados)
+    print(f"Dados inseridos com sucesso em {bd_nome}.{collection_nome}!")
+
+
+# Função de conexão
+def conexao(bd_nome, collection_nome):
+    from pymongo import MongoClient
+    cliente = MongoClient('127.0.0.1:27017')
+    bd = cliente[bd_nome]
+    collection = bd[collection_nome]
+    return collection
